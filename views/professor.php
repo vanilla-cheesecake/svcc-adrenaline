7da0591957728ff7c23b542501959c6f5e346429
golez <golez.sf@gmail.com>
2023-03-24 16:46:35 +0800

    Professor UI dashboard
    Professor mock UI dashboard
    - Today's Schedule
    - Attendance Report

--- views/professor.php
+++ views/professor.php
@@ -1,93 +1,84 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SVCC ADRENALINE</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Professor Attendance</title>
 </head>
 <body>
-	<header>
-		<h1>SVCC ADRENALINE</h1>
-		<nav>
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Classes</a></li>
-				<li><a href="#">Attendance</a></li>
-				<li><a href="#">Reports</a></li>
-				<li><a href="#">Settings</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<section>
-			<h2>Welcome, Professor Lloyd</h2>
-			<p>You are currently checked in to Class ABC123.</p>
-			<form>
-				<input type="submit" value="Check Out">
-			</form>
-		</section>
-		<section>
-        <h2>Today's Schedule</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>Time</th>
-						<th>Class</th>
-						<th>Location</th>
-						<th>Check In</th>
-						<th>Check Out</th>
-					</tr>
-				</thead>
-				<tbody>
-                    <tr>
-						<td>8:00 AM</td>
-						<td>ABC123</td>
-						<td>Room 101</td>
-						<td><form><input type="submit" value="Check In"></form></td>
-						<td><form><input type="submit" value="Check Out"></form></td>
-					</tr>
-                    <tr>
-						<td>10:00 AM</td>
-						<td>DEF456</td>
-						<td>Room 202</td>
-						<td><form><input type="submit" value="Check In"></form></td>
-						<td><form><input type="submit" value="Check Out"></form></td>
-					</tr>
-				</tbody>
-			</table>
-		</section>
-		<section>
-			<h2>Attendance Report</h2>
-			<p>You have attended 10 out of 12 classes this semester.</p>
-			<table>
-				<thead>
-					<tr>
-						<th>Class</th>
-						<th>Date</th>
-						<th>Time</th>
-						<th>Status</th>
-						<th>Location</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>ABC123</td>
-						<td>January 15, 2023</td>
-						<td>8:00 AM</td>
-						<td>Present</td>
-						<td>Room 101</td>
-					</tr>
-					<tr>
-						<td>ABC123</td>
-						<td>January 22, 2023</td>
-						<td>8:00 AM</td>
-						<td>Present</td>
-						<td>Room 101</td>
-					</tr>
-					<!-- More rows for attendance records -->
-				</tbody>
-			</table>
-		</section>
-	</main>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Classes</a></li>
+			<li><a href="#">Attendance</a></li>
+			<li><a href="#">Reports</a></li>
+			<li><a href="#">Settings</a></li>
+		</ul>
+	</nav>
+	<h1>Professor Attendance</h1>
+	<h2>Today's Date: March 24, 2023</h2>
+	<h2>Upcoming Class:</h2>
+	<p>Class Name: Math 101</p>
+	<p>Room: 201</p>
+	<p>Location: Building A</p>
+	<p>Start Time: 9:00 AM</p>
+	<p>End Time: 10:30 AM</p>
+	<button>Start Class</button>
+	<button>End Class</button>
+	<h2>Class Schedule for March 24, 2023:</h2>
+	<table>
+		<tr>
+			<th>Class Name</th>
+			<th>Room</th>
+			<th>Location</th>
+			<th>Start Time</th>
+			<th>End Time</th>
+			<th>Units</th>
+			<th>Actions</th>
+		</tr>
+		<tr>
+			<td>Math 101</td>
+			<td>201</td>
+			<td>Building A</td>
+			<td>9:00 AM</td>
+			<td>10:30 AM</td>
+			<td>3</td>
+			<td>
+				<button>Start Class</button>
+				<button>End Class</button>
+			</td>
+		</tr>
+		<tr>
+			<td>English 201</td>
+			<td>301</td>
+			<td>Building B</td>
+			<td>11:00 AM</td>
+			<td>12:30 PM</td>
+			<td>3</td>
+			<td>
+				<button>Start Class</button>
+				<button>End Class</button>
+			</td>
+		</tr>
+		<tr>
+			<td>History 301</td>
+			<td>401</td>
+			<td>Building C</td>
+			<td>2:00 PM</td>
+			<td>3:30 PM</td>
+			<td>3</td>
+			<td>
+				<button>Start Class</button>
+				<button>End Class</button>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="5"><strong>Total Units:</strong></td>
+			<td><strong>9</strong></td>
+			<td></td>
+		</tr>
+	</table>
+	<h2>Attendance Report:</h2>
+	<p>Number of Total Units: 18/69</p>
+	<p>Total Classes Attended: 6 out of 9</p>
+	<p>Salary: X.XX (based on total hours worked and number of units taught)</p>
 </body>
 </html>
